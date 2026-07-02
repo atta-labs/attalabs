d19b9dc83241aa7f7435ed6719176782a8029ea1
Daniel Estevez <estevez.dani@gmail.com>
2026-07-02 17:15:03 +0200

    Feat(aeg): Add archive-task provenance assembly to aeg-core

--- packages/aeg-core/src/index.ts
+++ packages/aeg-core/src/index.ts
@@ -51,3 +51,5 @@ export {
   isGrandfathered
 } from './coherence-checks'
 export type { CheckFailure, CheckResult, IterationFile, TaskEntry } from './coherence-checks'
+export { buildProvenanceBlock, hasProvenance, taskRefFromBranch } from './archive-task'
+export type { MergedPrFacts } from './archive-task'
